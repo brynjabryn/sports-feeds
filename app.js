25331219b7259d9d7e76dceca0a6403a5b9a16ad
bkelly <bkelly@ibsys.com>
2015-04-19 11:00:22 -0500

    5: Styling and functionality changes

--- app.js
+++ app.js
@@ -1,24 +1,55 @@
 (function(){
 	var app = angular.module('sports-feeds', []);
 
-	var req = {		
+	var boxReq = {		
 		method: 'GET',
 		url: '/data/WI-DUKE-BOXSCORE.json'
+	}
+	
+	var pbpReq = {		
+		method: 'GET',
+		url: '/data/WI-DUKE-PBP.json'
 	}
 	
 	app.controller('WIDukeBoxController', ['$http', function($http) {
 		var wiDukeBox = this;
 		wiDukeBox.stats = [];
 			
-		$http(req)
+		$http(boxReq)
 			.success(function(data){
 				wiDukeBox.stats = data;
-				homePoints = wiDukeBox.stats.home.leaders.points[0];
-				console.log(homePoints);
+// 				homePoints = wiDukeBox.stats.home.leaders.points[0];
+// 				console.log(homePoints);
 			})
 			.error(function(response, status) {
 				console.log("The SportsData request failed with response " + response + " and status code " + status);
 			});
 	} ]);
+	
+	app.controller('WIDukePbpController', ['$http', function($http) {
+		var wiDukePbp = this;
+		wiDukePbp.stats = [];
+			
+		$http(pbpReq)
+			.success(function(data){
+				wiDukePbp.stats = data;
+				console.log(data);
+			})
+			.error(function(response, status) {
+				console.log("The SportsData request failed with response " + response + " and status code " + status);
+			});
+	} ]);
+	
+	app.controller('TabController', function() {
+		this.tab = 1;
+		
+		this.selectTab = function(setTab) {
+			this.tab = setTab;
+		};
+		
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		};
+	});
 	
 })();
