cd507af2b9e2b710730c4bc4519cba209da2fa99
bkelly <bkelly@ibsys.com>
2015-04-20 00:00:54 -0500

    :7: Play by play functioning

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 (function(){
 	var app = angular.module('sports-feeds', []);
 
-	var boxReq = {		
+	var wiDukeboxReq = {		
 		method: 'GET',
 		url: '/data/WI-DUKE-BOXSCORE.json'
 	}
 	
-	var pbpReq = {		
+	var wiDukepbpReq = {		
 		method: 'GET',
 		url: '/data/WI-DUKE-PBP.json'
 	}
@@ -15,11 +15,9 @@
 		var wiDukeBox = this;
 		wiDukeBox.stats = [];
 			
-		$http(boxReq)
+		$http(wiDukeboxReq)
 			.success(function(data){
 				wiDukeBox.stats = data;
-// 				homePoints = wiDukeBox.stats.home.leaders.points[0];
-// 				console.log(homePoints);
 			})
 			.error(function(response, status) {
 				console.log("The SportsData request failed with response " + response + " and status code " + status);
@@ -30,14 +28,54 @@
 		var wiDukePbp = this;
 		wiDukePbp.stats = [];
 			
-		$http(pbpReq)
+		$http(wiDukepbpReq)
 			.success(function(data){
 				wiDukePbp.stats = data;
-				console.log(data);
+				pbpStatsEvents(wiDukePbp.stats);
 			})
 			.error(function(response, status) {
 				console.log("The SportsData request failed with response " + response + " and status code " + status);
 			});
+		
+		function pbpStatsEvents(stats) {
+			var firstHalf = stats.periods[0],
+				secondHalf = stats.periods[1],
+				firstEventsLength = firstHalf.events.length,
+				secondEventsLength = secondHalf.events.length,
+				i;
+			
+			wiDukePbp.stats.firstHalfArray = [];
+			wiDukePbp.stats.secondHalfWiArray = [];
+			
+			for (i = 0; i < firstEventsLength; i++) {
+				var description = "(" + (i+1) + ") " + firstHalf.events[i].description;
+					
+				if (firstHalf.events[i].possession != undefined) {
+					var possession = firstHalf.events[i].possession.market;	
+				}
+				
+				if (possession == "Wisconsin") {
+					var event = {
+						wiDescription: description,
+						clock: firstHalf.events[i].clock,
+						dukeDescription: "(" + (i+1) + ")" 	
+					};
+					wiDukePbp.stats.firstHalfArray.push(event);									
+				} else if (possession == "Duke") {
+					var event = {
+						wiDescription: "(" + (i+1) + ")",
+						clock: firstHalf.events[i].clock,
+						dukeDescription: description 	
+					};
+					wiDukePbp.stats.firstHalfArray.push(event);
+
+				} else {
+					console.log("Unable to find possession for " + firstHalf.events[i]);
+				}	
+				
+			}
+						
+		}
 	} ]);
 	
 	app.controller('TabController', function() {
